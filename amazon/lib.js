b94d06cf8db0ae906d7a4231c71c622c23267389
Yixing Lin <184059914@qq.com>
2023-09-22 04:42:33 +0200

    Add button to order page

--- amazon/lib.js
+++ amazon/lib.js
@@ -11,7 +11,7 @@ function checkZipCode(postalCode) {
     }
 }
 
-// Example: Muster Strasse 123a
+// Example: Muster Straße 123a
 function checkStreet(street) {
     var reg = /^([A-Za-zäößüÄÜÖ\s.,-]+)\s([\d]+[a-zA-Z]?)$/
     if(reg.test(street)){
@@ -25,7 +25,7 @@ function checkStreet(street) {
     }
 }
 
-// Example: Muster Strasse | 123a
+// Example: Muster Straße | 123a
 function splitStreet(street) {
     if (!checkStreet(street)) {
         return null;
@@ -33,3 +33,17 @@ function splitStreet(street) {
         return [RegExp.$1, RegExp.$2];
     }
 }
+
+// Message show up
+function Toast(msg, duration){  
+    duration=isNaN(duration)?3000:duration;  
+    var m = document.createElement('div');  
+    m.innerHTML = msg;  
+    m.style.cssText="font-size: .32rem;color: rgb(255, 255, 255);background-color: rgba(0, 0, 0, 0.6);padding: 10px 15px;margin: 0 0 0 -60px;border-radius: 4px;position: fixed;    top: 50%;left: 50%;width: 430px;text-align: center;";
+    document.body.appendChild(m);  
+    setTimeout(function() {  
+        var d = 0.5;
+        m.style.opacity = '0';  
+        setTimeout(function() { document.body.removeChild(m) }, d * 1000);  
+    }, duration);  
+}  
--- amazon/order.js
+++ amazon/order.js
@@ -5,9 +5,23 @@ function testAlert() {
 
 class GermanLike {
     constructor(dom) {
+        this.countryMap = {"Amazon.de": "de"};
         this.dom = dom.querySelector('div[data-test-id="shipping-section-buyer-address"]');
+        this.dom2 = dom.querySelector('table.a-keyvalue');
+        this.dom3 = dom.querySelector('span[data-test-id="order-summary-sales-channel-value"]');
+        this.dom4 = dom.querySelector('div[data-test-id="order-details-header-action-buttons"]');
     }
 
+    addButton() {
+        // const ele = '<span data-test-id="clipboard-button" class="a-button"><span class="a-button-inner"><input class="a-button-input" type="submit" value="复制到剪切板"><span class="a-button-text" aria-hidden="true">复制到剪切板</span></span></span>'
+        const ele = '<span class="a-button-inner"><input class="a-button-input" type="submit" value="复制客户信息"><span class="a-button-text" aria-hidden="true">复制客户信息</span></span>'
+        var buttonBar = this.dom4;
+        var cbBtn = buttonBar.childNodes[0].cloneNode(true);
+        cbBtn.setAttribute("data-test-id", "clipboard-button");
+        cbBtn.innerHTML = ele;
+        buttonBar.insertBefore(cbBtn, buttonBar.childNodes[0]);
+        return cbBtn;
+    }
     parse() {
         const lines = this.dom.childNodes;
         var pureLines = [];
@@ -15,9 +29,13 @@ class GermanLike {
             pureLines.push(lines[i].innerText.trim());
         }
         pureLines.reverse();
-        const ele = pureLines[1]
+        var ele = pureLines[1]
         if (ele.indexOf(",") != -1) {
             pureLines.splice(1, 0, null)
+        }
+        if (pureLines.length > 6) {
+            alert("Items > 6");
+            throw new Error('Items > 6!');
         }
         console.log(pureLines);
         
@@ -42,8 +60,30 @@ class GermanLike {
             shipment.houseNumber = st[1];
         }   
 
+        if (pureLines.length == 5) {
+            shipment.name1 = pureLines[4];
+            shipment.name2 = null;
+            shipment.name3 = null;
+        } else if (pureLines.length == 6) {
+            shipment.name1 = pureLines[4];
+            shipment.name2 = pureLines[5];
+            shipment.name3 = null;
+        }
+
+        const quantity = this.dom2.querySelectorAll('td')[4].innerText;
+        const note = this.dom2.querySelectorAll("div.product-name-column-word-wrap-break-all")[1].innerText
+        shipment.note = quantity + note.replace("SKU", "").replace(":", "x").trim();
+        const channel = this.dom3.textContent.trim();
+        console.log(channel)
+        shipment.country = this.countryMap[channel];
+        if (shipment.country == undefined) {
+            throw new Error('Country not in the whitelist');
+        }
         return shipment;
     }
 
 }
+
+
+
 
