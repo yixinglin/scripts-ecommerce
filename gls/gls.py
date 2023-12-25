f2a505050d989dee0e1368a5de78e916581c6d95
Yixing Lin <184059914@qq.com>
2023-12-25 22:19:59 +0100

    Add document properties to pdf.
    Add document properties to pdf
    Bug fixed regarding utf-8 encoding of logging.

--- gls/gls.py
+++ gls/gls.py
@@ -3,20 +3,22 @@ import logging
 import glo
 import base64
 import requests
-import json 
+import json
 from typing import List, Dict
 import platform
 from pylib.ioutils import *
+
 PTH_PAYLOAD = "./gls/payload.json"
 PTH_CONF = "./gls/config.yaml"
 OS_TYPE = platform.system()
+
 
 class GLSApi:
     MAX_NAME_LEN = 37
 
     def __init__(self, url, username, password, shipperId) -> None:
         self.app = glo.getValue("app")
-        self.url = url 
+        self.url = url
         self.shipperId = shipperId
         auth = f"{username}:{password}"
         auth = base64.b64encode(auth.encode()).decode()
@@ -28,37 +30,45 @@ class GLSApi:
             "Content-Type": "application/json",
             "Authorization": f"Basic {auth}"
         }
-          
-    def createParcelLabel(self, payload:dict, addictionText:str="") -> dict:
+
+    def createParcelLabel(self, payload: dict, addictionText: str = "") -> dict:
         postUrl = self.url + "/shipments"
         logging.info("[GLS REQUEST]: " + postUrl)
-        resp = requests.post(postUrl, headers=self.headers, json=payload) 
-        if (resp.status_code == 201):
+        resp = requests.post(postUrl, headers=self.headers, json=payload)
+        if resp.status_code == 201:
             parcel = json.loads(resp.text)
             parcelWithWatermark = self.addTextToParcel(parcel['labels'][0], addictionText)
+            # Set Document Properties
+            props = DocumentProperties()
+            props.title = payload['references'][0]
+            props.subject = "GLS Parcel Label"
+            props['id'] = payload['references'][0]
+            props['addresses'] = json.dumps(payload['addresses'])
+            props['parcels'] = json.dumps(payload['parcels'])
+            # parcelWithWatermark = setPdfDocumentProperties(parcelWithWatermark, props)
+            # Modify result from gls
             parcel['labels'][0] = parcelWithWatermark
-            return parcel 
+            return parcel
         else:
             raise Exception(f"[GLS] Exception {resp.status_code}.\n{resp.text}")
-    
-    def addTextToParcel(self, b64Parcel:str, text:str):
-        pagesize = getPdfPageSize(b64Parcel) # Pagesize of the parcel
+
+    def addTextToParcel(self, b64Parcel: str, text: str):
+        pagesize = getPdfPageSize(b64Parcel)  # Pagesize of the parcel
         b64Watermark = createPdfWatermark(text, pagesize=pagesize,
-                                    textPosition=(10*mm, 75*mm),
-                                    textColor=(0, 0, 0, 0.9),
-                                    fontSize=7)
+                                          textPosition=(10 * mm, 75 * mm),
+                                          textColor=(0, 0, 0, 0.9),
+                                          fontSize=7)
         b64out = addWatermarkToPdf(b64Parcel, b64Watermark)
         return b64out
 
-
-    def fillForm(self, reference, name1, name2, name3, 
-                 street, city, zipCode, province, country, email, phone, parcels:List[Dict]):
+    def fillForm(self, reference, name1, name2, name3,
+                 street, city, zipCode, province, country, email, phone, parcels: List[Dict]):
         name1, name2, name3 = self.adjustNameFields(name1, name2, name3)
         passed = self.checkNameLength([name1, name2, name3])
-        if(not passed):
+        if (not passed):
             raise Exception("Names are too long.")
 
-        with open(PTH_PAYLOAD, "r", encoding="utf-8") as f:  
+        with open(PTH_PAYLOAD, "r", encoding="utf-8") as f:
             payload = json.load(f)
         payload["shipperId"] = self.shipperId
         payload["references"] = [reference]
@@ -73,13 +83,13 @@ class GLSApi:
         payload["addresses"]["delivery"]["email"] = email
         payload["addresses"]["delivery"]["phone"] = phone
         payload["parcels"] = parcels
-        return payload 
+        return payload
 
     def checkNameLength(self, names: List[str]):
         for name in names:
             if len(name) > self.MAX_NAME_LEN:
                 return False
-        return True 
+        return True
 
     def adjustNameFields(self, name1, name2, name3):
         tmp = ""
@@ -87,16 +97,15 @@ class GLSApi:
             tmp = name1[self.MAX_NAME_LEN:]
             name1 = name1[:self.MAX_NAME_LEN]
             if (len(name3) > 0):
-                name2 = tmp + " || " +name2
+                name2 = tmp + " || " + name2
             else:
                 name3 = name2
                 name2 = tmp
         if (len(name2) >= self.MAX_NAME_LEN):
             tmp = name2[self.MAX_NAME_LEN:]
             name2 = name2[:self.MAX_NAME_LEN]
-            if(len(name3) > 0):
-                name3 = tmp + " || " + name3 
+            if (len(name3) > 0):
+                name3 = tmp + " || " + name3
             else:
                 name3 = tmp
         return [name1, name2, name3]
-            
