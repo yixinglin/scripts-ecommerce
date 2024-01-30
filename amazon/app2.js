3b8be54abd46b822dbf5e95345fe3e0e0d627dab
Yixing Lin <184059914@qq.com>
2024-01-30 13:56:42 +0100

    Sorte orders by ASIN

--- amazon/app2.js
+++ amazon/app2.js
@@ -17,8 +17,10 @@ class AmazonOrderListApplication {
             this.surface = new OrderListPageSurface(this.dom);
             this.surface.addButtonBulkConfirmShipmentV2().on("click", () => {
                 const orders = this.surface.getSelectedOrderNumbers();   // Order sorted by asin
-                var links = this.surface.setConfirmShipmentLink(orders);   
-                window.open(links, '_blank').focus();
+                var linkConfirmShipment = this.surface.setConfirmShipmentLink(orders);   
+                var linkPackShipment = this.surface.setPackSlipLink(orders);
+                window.open(linkConfirmShipment, '_blank');
+                window.open(linkPackShipment, '_blank').focus();
             })
         });
 
@@ -41,8 +43,9 @@ class OrderListPageSurface {
         this.dom = dom;
         this.confirmShipmentButton = null;   
         this.link_confirmShipments = "https://sellercentral.amazon.de/orders-v3/bulk-confirm-shipment";
+        this.link_packSlips = "https://sellercentral.amazon.de/orders/packing-slip?orderIds=";
     }
-
+    // Create Button of bulk confirming orders.
     addButtonBulkConfirmShipmentV2() {
         const name = "分类并确认送货[1]";
         const ele = `<span class="a-button-inner"><input class="a-button-input" type="submit" value=${name} aria-labelledby="a-autoid-10-announce"><span class="a-button-text" aria-hidden="true">${name}</span></span>`;
@@ -57,9 +60,10 @@ class OrderListPageSurface {
         csBtn.after(this.confirmShipmentButton);
         return this.confirmShipmentButton;
     }
-
+    // Get a list of seleted orders.
     getSelectedOrderNumbers() {
         var listOrder = [];        
+        // Get orders number from table;
         $("#orders-table > tbody > tr").each((index) => {
             const a = $(`#orders-table > tbody > tr:nth-child(${index+1}) > td:nth-child(3) > div > div.cell-body-title > a`);
             var odn = a.text().trim();
@@ -67,14 +71,21 @@ class OrderListPageSurface {
             var asin = asin.text().trim();
             listOrder.push({"orderNumber": odn, "asin": asin});
         });
-
+        // Sort orders by asin
         listOrder.sort((a, b) => {
             if(a.asin > b.asin) return 1;
             if(a.asin < b.asin) return -1;
             return 0;
         })
-
-        return listOrder.map(o => o.orderNumber);
+        // Get selected orders
+        var link = $('span[data-test-id="ab-bulk-buy-shipping"] > span > a').attr('href');
+        var selectedOrders = link.split(';');
+        selectedOrders = selectedOrders.filter(o => o !== "" && o !== null);
+        selectedOrders[0] = selectedOrders[0].split("=")[1];
+        // Get selected orders sort by asin
+        var res = listOrder.map(o => o.orderNumber)
+                            .filter(o =>selectedOrders.includes(o));
+        return res;
     }
 
     setConfirmShipmentLink(orders) {
@@ -83,14 +94,20 @@ class OrderListPageSurface {
         return link
     }
 
+    setPackSlipLink(orders) {
+        const link = this.link_packSlips  + orders.join(";") + ";";
+        console.log(link);
+        return link;
+    }
+
 }
 
 function insertConfirmShipmentToLinks() {  
+    // Insert link to Confirm-Shipment-Button
     $("#bulk-confirm-orders-table > tbody > tr").each((index) => {
-        var tag = $(`#bulk-confirm-orders-table > tbody > tr:nth-child(${index}) > td:nth-child(1) > div > div.cell-body-title > a`)
+        var tag = $(`#bulk-confirm-orders-table > tbody > tr:nth-child(${index+1}) > td:nth-child(1) > div > div.cell-body-title > a`)
         var a = tag.attr("href");
         tag.attr("href", a+"/confirm-shipment")
-        debugger
     })
     
 }
